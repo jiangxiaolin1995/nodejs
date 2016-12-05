98de9819123eb60acf9b7a36b9770c3c0a0ce4e2
jiangxiaolin1995 <jiangxiaolin1995@gmail.com>
2016-12-05 21:46:58 +0800

    add http
    http基础

--- node/url.js
+++ node/url.js
@@ -5,3 +5,5 @@
 //url.resolve('/one/two/three', 'four')         // '/one/two/four'
 //url.resolve('http://example.com/', '/one')    // 'http://example.com/one'
 //url.resolve('http://example.com/one', '/two') // 'http://example.com/two'
+
+//url.prase('http://www.imooc.com/video/6710','')
