657b92c966b422c539fc3901bc7a596446bfe651
jiangxiaolin1995 <jiangxiaolin1995@gmail.com>
2016-12-06 21:36:28 +0800

    add

---
+++ node/http/async.js
@@ -0,0 +1,26 @@
+// var c = 0;
+// function printIn(){
+//   console.log(c);
+// }
+// function plus() {
+//  setTimeout(function () {
+//     c += 1;
+//  }, 1000);
+// }
+// plus();
+// printIn()
+
+var c = 0;
+
+function printIt() {
+  console.log(c);
+}
+
+function plus(callback) {
+  setTimeout(function () {
+    c += 1;
+    callback();
+  }, 1000);
+
+}
+plus(printIt);
